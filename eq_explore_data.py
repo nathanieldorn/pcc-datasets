d757f3953399bc3a528e82e6e99732dea44b9e34
nathanieldorn <nathaniel.svc@pm.me>
2026-01-21 19:48:32 -0500

    Extracts of magnitude, longitude, & latitude + initial location plot

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -1,13 +1,19 @@
 import json
 from pathlib import Path
 
+import plotly.express as px
+
 
 def read_json():
     """Read data from json files within eq_data directory"""
     path = Path("eq_data/eq_data_1_day_m1.geojson")
     contents = path.read_text(encoding="utf-8")
     eq_data = json.loads(contents)
-    return eq_data
+    # return eq_data
+
+    # examine earthquakes in the dataset
+    eq_dicts = eq_data["features"]
+    return eq_dicts
 
 
 def readable_json():
@@ -15,3 +21,25 @@ def readable_json():
     path = Path("eq_data/readable_eq_data.geojson")
     readable_contents = json.dumps(read_json(), indent=4)
     path.write_text(readable_contents)
+
+
+def get_magnitudes():
+    """Creates a list of earthquake magnitudes"""
+    eq_dicts = read_json()
+    magnitudes = [earthquake["properties"]["mag"] for earthquake in eq_dicts]
+    return magnitudes
+
+
+def get_location_data():
+    """Creates a list of longitudinal and latitudinal coordinates, returning a list for each"""
+    eq_dicts = read_json()
+    longitudes = [earthquake["geometry"]["coordinates"][0] for earthquake in eq_dicts]
+    latitudes = [earthquake["geometry"]["coordinates"][1] for earthquake in eq_dicts]
+    return longitudes, latitudes
+
+
+def earthquake_world_map():
+    longitudes, latitudes = get_location_data()
+    title = "Global Earthquakes"
+    fig = px.scatter_geo(lat=latitudes, lon=longitudes, title=title)
+    fig.show()
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from death_valley_highs_lows import plot_temperatures_death_valley
-from eq_explore_data import readable_json
+from eq_explore_data import (
+    earthquake_world_map,
+    get_location_data,
+    get_magnitudes,
+    read_json,
+    readable_json,
+)
 from sitka_highs import plot_temperatures
 
 
@@ -11,7 +17,9 @@ def main():
     # plot_temperatures_death_valley()
 
     # plot for earthquake data
-    readable_json()
+    # readable_json()
+    # get_magnitudes()
+    earthquake_world_map()
 
 
 if __name__ == "__main__":
